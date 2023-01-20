abb802b2bcaf3c8255decf7862e358f56e71cda8
zohaibzyebi <zyebi553@gmail.com>
2023-01-21 01:13:00 +0500

    add: favicon + title updated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,9 +22,9 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   return (
     <>
       <Head>
-        <title>Mantine-Tailwind-Next Example</title>
+        <title>Our Campgrounds | Spacius Skies Campgrounds</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favlogo.svg" />
       </Head>
 
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
